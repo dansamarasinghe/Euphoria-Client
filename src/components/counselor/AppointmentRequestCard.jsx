93be17401d80115f015aecf080cfd4cbe4f54a0b
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-01-22 07:25:22 +0530

    card

--- src/components/counselor/AppointmentRequestCard.jsx
+++ src/components/counselor/AppointmentRequestCard.jsx
@@ -6,3 +6,36 @@ import {updateAppointmentStatus} from "../../actions/counselor/counselorActions"
 const axios = require('axios').default;
 
 // import { connect } from 'react-redux';
+
+class AppointmentRequestCard extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            appointment: props.appointment,
+            key: props.keyValue,
+            status: props.appointment.status
+        };
+        // console.log(this.state.appointment)
+    }
+
+    onApprove = () => {
+        let appointment = this.state.appointment;
+        appointment.status = "ACCEPTED";
+        console.log(appointment);
+        axios.post('http://localhost:8080/api/counselor/appointments',
+            appointment,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            }
+        ).then((response) => {
+            this.setState({
+                status: "ACCEPTED",
+                disableActions: true,
+            })
+        }).catch((err) => {
+            console.log(err);
+        })
+    };
