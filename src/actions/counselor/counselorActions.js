9f58c88256dce99a393be66aa23e0460e8349063
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-01-22 07:05:21 +0530

    jwt

--- src/actions/counselor/counselorActions.js
+++ src/actions/counselor/counselorActions.js
@@ -3,3 +3,14 @@ import * as actionTypes from '../types';
 const axios = require('axios').default;
 
 // Check if user is logged in.
+(function () {
+    let authToken = localStorage.getItem("JWT");
+    if (authToken === null) {
+        // This means that there ISN'T JWT and no user is logged in.
+        axios.defaults.headers.common.Authorization = null;
+    } else {
+        // This means that there IS a JWT so someone must be logged in.
+        axios.defaults.headers.common.Authorization = `Bearer ${authToken}`;
+    }
+})();
+
