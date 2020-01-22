eb7956ef19298f042ed7aace48cebb93c88cf9c4
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-01-22 07:28:20 +0530

    cardstart

--- src/components/counselor/AppointmentRequestCard.jsx
+++ src/components/counselor/AppointmentRequestCard.jsx
@@ -106,3 +106,56 @@ class AppointmentRequestCard extends Component {
 
         return (
             <Grid item xs={6}>
+
+                {/*Card-Start*/}
+                <Card key={this.props.keyValue}>
+                    <CardHeader
+                        title={this.state.appointment.id.user.firstname + " " + this.state.appointment.id.user.lastname}
+                        subheader={this.state.appointment.id.user.email}
+                    />
+                    <hr/>
+                    <CardContent>
+                        <Grid container spacing={2}>
+                            <Grid item xs={4}>
+                                <Grid item xs={12}>
+                                    <Typography color={"textSecondary"} style={{fontSize: "1em"}}>
+                                        Requested on {createdAt[0]}-{createdAt[1]}-{createdAt[2]}
+                                    </Typography>
+                                    <Typography color={"textSecondary"} style={{fontSize: "1em"}}>
+                                        At {createdAt[3]}:{createdAt[4]}
+                                    </Typography>
+                                </Grid>
+                                {/*<Grid item xs={12}>*/}
+                                {/*    <Typography style={{fontSize: "1em"}}>*/}
+
+                                {/*    </Typography>*/}
+                                {/*</Grid>*/}
+
+                                <Grid item xs={12}>
+                                    <Typography style={{fontSize: "1em"}}>
+                                        {this.state.status}
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+
+                            <Grid item xs={8}>
+                                <Typography variant={"body1"} color={"textPrimary"}>
+                                    {this.state.appointment.description}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </CardContent>
+
+                    <CardActions style={{float: "right"}}>
+                        {/*<Button onClick={this.handleClick}>Approve</Button>*/}
+                        {actions}
+                    </CardActions>
+                </Card>
+                {/*Card-End*/}
+
+            </Grid>
+        );
+    };
+}
+
+export default AppointmentRequestCard;
