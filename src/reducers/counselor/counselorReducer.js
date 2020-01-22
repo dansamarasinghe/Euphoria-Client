afce8554f84c14446f335c7c1adc6fea13fe320d
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-01-22 07:35:02 +0530

    reducer

--- src/reducers/counselor/counselorReducer.js
+++ src/reducers/counselor/counselorReducer.js
@@ -1,1 +1,35 @@
+import * as actionTypes from '../../actions/types';
 
+const initialState = {
+    signedIn: false,
+    patientRecords: [],
+    appointments:[],
+};
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case  actionTypes.COUNSELOR_SIGN_IN:
+            return {
+                ...state,
+                signedIn: true
+            };
+        case actionTypes.GET_PATIENT_RECORDS:
+            return {
+                ...state,
+                patientRecords: action.patientRecords
+            };
+        case actionTypes.GET_APPOINTMENTS:
+            return {
+                ...state,
+                appointments: action.appointments
+            };
+
+        case actionTypes.GET_APPOINTMENTS:
+            return {
+                ...state,
+                // appointments: action.appointments
+            };
+        default:
+            return state;
+    };
+};
