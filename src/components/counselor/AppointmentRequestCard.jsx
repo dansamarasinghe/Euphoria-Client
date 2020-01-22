414baff037007985d362ec27a8095e268a60b133
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-01-22 07:24:52 +0530

    axios

--- src/components/counselor/AppointmentRequestCard.jsx
+++ src/components/counselor/AppointmentRequestCard.jsx
@@ -2,3 +2,7 @@ import React, {Component} from 'react'
 import {Button, Card, CardActions, CardContent, CardHeader, Grid} from '@material-ui/core'
 import Typography from '@material-ui/core/Typography';
 import {updateAppointmentStatus} from "../../actions/counselor/counselorActions";
+
+const axios = require('axios').default;
+
+// import { connect } from 'react-redux';
