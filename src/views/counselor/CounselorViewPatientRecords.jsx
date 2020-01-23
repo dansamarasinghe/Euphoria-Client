08db12a67b3665c74e141846e6e32f126db5bc2c
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-01-23 07:46:35 +0530

    render

--- src/views/counselor/CounselorViewPatientRecords.jsx
+++ src/views/counselor/CounselorViewPatientRecords.jsx
@@ -36,4 +36,48 @@ class CounselorViewPatientRecords extends Component {
         })
     };
 
-    
+    render() {
+        return (
+            <>
+
+                <CounselorNavBar></CounselorNavBar>
+
+                <Grid container direction={"column"} style={{padding: "1em"}}>
+                    <Grid container>
+                        <Grid style={{marginBottom: "1em"}}>
+                            <Typography variant={"h1"} style={{fontSize: "2em"}}>
+                                {"Patient Records"}
+                            </Typography>
+                            {/*<Typography variant={"h2"} style={{fontSize: "1em"}}>
+                                {"Missaka Iddamalgoda"}
+                            </Typography>*/}
+                        </Grid>
+                    </Grid>
+                    <Grid container direction={"row"} spacing={1}>
+                        <Grid item xs={12} sm={3}>
+                            {/** TODO: pass props to these  */}
+                            <ProfileInfoCard ></ProfileInfoCard>
+                        </Grid>
+                        <Grid item xs={12} sm={9}>
+                            {this.state.records !== null ?
+                                this.state.records.map((record, key) => (
+                                    <>
+
+                                        {/*Card-Start*/}
+                                        <PatientRecordCard record={record}/>
+                                        {/*<AppointmentRequestCard keyValue={key} appointment={appointment}/>*/}
+                                        {/*Card-End*/}
+
+                                    </>
+                                )) : <h3 style={{textAlign: 'center'}}> Loading...</h3>
+                            }
+                        </Grid>
+                    </Grid>
+                </Grid>
+
+            </>
+        );
+    };
+
+}
+
