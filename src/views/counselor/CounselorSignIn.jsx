035ac2e12b5b49565bf1d0838c236ef42fc45f6e
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-01-23 07:39:22 +0530

    import

---
+++ src/views/counselor/CounselorSignIn.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import "./CounselorSignIn.scss";
+import {Grid, Card, CardContent, TextField, Typography, Button} from "@material-ui/core";
+import CloudUploadIcon from "@material-ui/icons/CloudUploadOutlined";
+import logo from "../../assets/eu-logo.png";
+import {connect} from 'react-redux';
+import * as actions from "../../actions/index";
+
+const validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+const validateForm = (errors) => {
+    let valid = true;
+    Object.values(errors).forEach(
+        (val) => val.length > 0 && (valid = false)
+    );
+    return valid;
+};
