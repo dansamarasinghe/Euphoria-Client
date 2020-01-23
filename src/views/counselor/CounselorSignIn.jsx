65b6d054a4bc31341a3bb78ea6b228a332a9966c
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-01-23 07:43:24 +0530

    render

--- src/views/counselor/CounselorSignIn.jsx
+++ src/views/counselor/CounselorSignIn.jsx
@@ -152,3 +152,292 @@ class CounselorSignIn extends Component {
             this.props.history.push("/counselor/appointments");
         }
     };
+
+    render() {
+        const {errors} = this.state;
+
+        const error = {
+            color: '#db2269',
+            fontSize: '0.825em',
+            display: 'relative'
+        };
+
+        let card
+        if (!this.state.isSignUp) {
+            card =
+                <>
+                    {/* Begin:Sign-In Card */}
+                    <Grid id={"CrdSgnIn"} item xs={12} sm={8} md={6} lg={4}>
+                        <Card className={'transparent'}>
+                            <CardContent>
+                                <Grid container id={'logoGrid'} className={"imgRaised"}>
+                                    <Grid item>
+                                        <img src={logo}/>
+                                    </Grid>
+
+                                    <Grid item xs={12} className={'brandText'}>
+                                        <Typography>Euphoria</Typography>
+                                    </Grid>
+                                </Grid>
+                                <hr/>
+                                <Grid container id={'fieldGrid'} spacing={2}>
+                                    <Grid item xs={8}>
+                                        <TextField id={'loginUsername'}
+                                                   onChange={e => this.handleChange(e)}
+                                                   className={'txtFld-small'}
+                                                   variant={'outlined'}
+                                                   label={'Username'}
+                                                   fullWidth
+                                                   noValidate
+                                        />
+                                        {errors.loginUsername.length === 0 &&
+                                        <span style={error}>{errors.loginUsername}</span>}
+                                    </Grid>
+
+                                    <Grid item xs={8}>
+                                        <TextField
+                                            id={'loginPassword'}
+                                            onChange={e => this.handleChange(e)}
+                                            className={'txtFld-small'}
+                                            variant={'outlined'}
+                                            type={'password'}
+                                            label={'Password'}
+                                            fullWidth/>
+                                        {errors.loginPassword.length > 0 &&
+                                        <span style={error}>{errors.loginPassword}</span>}
+                                    </Grid>
+
+                                    <Grid item xs={12} sm={10} id={'buttonGrid'}
+                                          className={'verticalCenter spaceBetween'}>
+                                        <Button> Forgot your password?</Button>
+                                        <Button
+                                            variant={"outlined"}
+                                            color={"primary"}
+                                            id={'signInBtn'}
+                                            type={"submit"}
+                                            onClick={() => this.signIn()}
+                                        >
+                                            Sign In
+                                        </Button>
+                                    </Grid>
+
+                                    <Grid item xs={12} className={'horizontalCenter'}>
+                                        <Button className={'btn-small'} onClick={() => this.changeMode()}>Not a member?
+                                            Join as a counselor
+                                            today.</Button>
+
+                                    </Grid>
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    {/* End:Sign-In Card */}
+                </>
+        } else {
+            card =
+                <>
+                    {/* Begin:Sign-Up Card */}
+                    <Grid id={"CrdSgnUp"} item xs={12} sm={8} md={6} lg={4}>
+                        <Card className={'transparent'}>
+                            <CardContent>
+                                <Grid container id={'logoGrid'} className={"imgRaised"}>
+                                    <Grid item xs={6}>
+                                        <img src={logo}/>
+                                    </Grid>
+
+                                    <Grid item xs={6} className={'brandText'}>
+                                        <Typography>Euphoria</Typography>
+                                    </Grid>
+
+                                </Grid>
+                                <hr/>
+                                <Grid container id={'fieldGrid'} spacing={2}>
+                                    <Grid item xs={12}>
+                                        <TextField id={'name'}
+                                                   onChange={e => this.handleChange(e)}
+                                                   className={'txtFld-small'}
+                                                   variant={'outlined'}
+                                                   label={'Name'}
+                                                   fullWidth
+                                                   type={'text'}
+                                                   noValidate
+                                        />
+                                        {errors.name.length > 0 &&
+                                        <span style={error}>{errors.name}</span>}
+                                    </Grid>
+
+                                    <Grid item xs={12}>
+                                        <TextField id={'email'}
+                                                   onChange={e => this.handleChange(e)}
+                                                   className={'txtFld-small'}
+                                                   variant={'outlined'}
+                                                   label={'Email'}
+                                                   fullWidth
+                                                   type={'email'}
+                                        />
+                                        {errors.email.length > 0 &&
+                                        <span style={error}>{errors.email}</span>}
+                                    </Grid>
+
+                                    <Grid item xs={12}>
+                                        <TextField id={'slmcNumber'}
+                                                   onChange={e => this.handleChange(e)}
+                                                   className={'txtFld-small'}
+                                                   variant={'outlined'}
+                                                   label={'SLMC Number'}
+                                                   fullWidth
+                                                   type={'text'}
+                                        />
+                                        {errors.slmcNumber.length > 0 &&
+                                        <span style={error}>{errors.slmcNumber}</span>}
+                                    </Grid>
+
+                                    <Grid item xs={6}>
+                                        <TextField
+                                            id={'speciality'}
+                                            onChange={e => this.handleChange(e)}
+                                            className={'txtFld-small'}
+                                            variant={'outlined'}
+                                            label={'Specialized Field'}
+                                            fullWidth/>
+                                    </Grid>
+
+                                    <Grid item xs={6}>
+                                        <TextField
+                                            id={'hospital'}
+                                            onChange={e => this.handleChange(e)}
+                                            className={'txtFld-small'}
+                                            variant={'outlined'}
+                                            label={'Hospital/Work'}
+                                            fullWidth/>
+                                    </Grid>
+
+                                    <Grid item xs={6}>
+                                        <TextField
+                                            id={'city'}
+                                            onChange={e => this.handleChange(e)}
+                                            className={'txtFld-small'}
+                                            variant={'outlined'}
+                                            label={'City'}
+                                            fullWidth/>
+                                    </Grid>
+
+                                    <Grid item xs={6}>
+                                        <input
+                                            accept="image/*"
+                                            id="picName"
+                                            type="file"
+                                            onChange={e => this.handleChange(e)}
+                                        />
+                                        <label htmlFor="picName">
+                                            <Button
+                                                variant="outlined"
+                                                component="span"
+                                                fullWidth={true}
+                                                size={"large"}
+                                                startIcon={<CloudUploadIcon/>}
+                                            >
+                                                Profile Photo
+                                            </Button>
+                                        </label>
+                                    </Grid>
+
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            id={'description'}
+                                            onChange={e => this.handleChange(e)}
+                                            className={'txtFld-small'}
+                                            variant={'outlined'}
+                                            label={'Description'}
+                                            multiline
+                                            rowsMax={'3'}
+                                            fullWidth/>
+                                    </Grid>
+
+                                    {/*<Grid item xs={6}>*/}
+                                    {/*    <TextField*/}
+                                    {/*        id={'password'}*/}
+                                    {/*        onChange={e => this.handleChange(e)}*/}
+                                    {/*        // className={'txtFld-small'}*/}
+                                    {/*        variant={'outlined'}*/}
+                                    {/*        label={'Password'}*/}
+                                    {/*        fullWidth*/}
+                                    {/*        type={'password'}*/}
+                                    {/*    />*/}
+                                    {/*</Grid>*/}
+
+                                    {/*<Grid item xs={6}>*/}
+                                    {/*    <TextField*/}
+                                    {/*        id={'confirm_password'}*/}
+                                    {/*        onChange={e => this.handleChange(e)}*/}
+                                    {/*        // className={'txtFld-small'}*/}
+                                    {/*        variant={'outlined'}*/}
+                                    {/*        label={'Confirm Password'}*/}
+                                    {/*        fullWidth*/}
+                                    {/*        type={'password'}*/}
+                                    {/*    />*/}
+                                    {/*</Grid>*/}
+
+                                    <Grid item xs={6}>
+                                        <TextField id={'username'}
+                                                   onChange={e => this.handleChange(e)}
+                                                   className={'txtFld-small'}
+                                                   variant={'outlined'}
+                                                   label={'Username'}
+                                                   fullWidth
+                                        />
+                                    </Grid>
+
+                                    <Grid item xs={6}>
+                                        <TextField
+                                            id={'password'}
+                                            onChange={e => this.handleChange(e)}
+                                            className={'txtFld-small'}
+                                            variant={'outlined'}
+                                            type={'password'}
+                                            label={'Password'}
+                                            fullWidth/>
+                                    </Grid>
+
+                                    <Grid item xs={12} sm={10} id={'buttonGrid'}
+                                          className={'verticalCenter spaceBetween'}>
+                                        <Button
+                                            onClick={() => this.changeMode()}
+                                        >
+                                            Already a member. Sign In instead?
+                                        </Button>
+                                        <Button
+                                            variant={"outlined"}
+                                            color={"primary"}
+                                            id={'signUpBtn'}
+                                            type={"submit"}
+                                            onClick={() => this.signUp()}
+                                        >
+                                            Sign Up
+                                        </Button>
+                                    </Grid>
+
+
+                                    <Grid item xs={12} className={'horizontalCenter'}>
+                                        {/*<Button className={'btn-small'}>Not a member? Join as a counselor*/}
+                                        {/*    today.</Button>*/}
+
+                                    </Grid>
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    {/* End:Sign-In Card */}
+                </>
+        }
+        return (
+            <>
+                <Grid container id="rootGrid">
+                    {card}
+                </Grid>
+            </>
+        );
+    };
+}
+
+
