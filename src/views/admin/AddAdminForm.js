ce25a2825058afc1c505f1b1d39abeb241450938
NipuniYR <nipuniyr@gmail.com>
2020-02-16 00:16:00 +0530

    Add admin

--- src/views/admin/AddAdminForm.js
+++ src/views/admin/AddAdminForm.js
@@ -1,18 +1,42 @@
 import React, {Component} from 'react';
 import {Button,Form} from 'react-bootstrap';
 import Card from "../../components/admin/Card";
+import axios from 'axios';
 
 class AddAdminForm extends Component{
     constructor(props){
         super(props)
         this.state={
-
+            firstName:'',
+            lastName:'',
+            email:'',
+            adminUsername:'',
+            adminPassword:'',
+            activeStatus:''
         }
+        this.handleChange = this.handleChange.bind(this);
+        this.addAdmin = this.addAdmin.bind(this);
+    }
 
+    handleChange(event){
+        this.setState({
+            [event.target.name]:event.target.value
+        })
     }
+
     addAdmin(){
-        
+        axios.post("http://localhost:8080/api/admin/addAdmin",JSON.stringify(this.state),{
+            headers: {
+                'Content-Type': 'application/json',
+            },
+         }
+         ).then((res)=>{
+            console.log(res);
+         });
+        this.props.history.push("/admin/administrators");
+        window.location.reload(false);
     }
+
     render(){
         return(
             <div>
@@ -23,36 +47,41 @@ class AddAdminForm extends Component{
                 ctTableFullWidth
                 ctTableResponsive
                 content={
-                    <Form>
+                    <Form noValidate>
                         <Form.Group controlId="formBasicFirstName">
                             <Form.Label>First Name</Form.Label>
-                            <Form.Control type="name" placeholder="FirstName" />
+                            <Form.Control type="name" name="firstName" value={this.state.firstName} placeholder="FirstName" onChange={this.handleChange}/>
                         </Form.Group>
 
                         <Form.Group controlId="formBasicLastName">
                             <Form.Label>Last Name</Form.Label>
-                            <Form.Control type="name" placeholder="LastName" />
+                            <Form.Control type="name" name="lastName"   value={this.state.lastName} placeholder="LastName" onChange={this.handleChange}/>
                         </Form.Group>
 
                         <Form.Group controlId="formBasicEmail">
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" placeholder="Email" />
+                            <Form.Control type="email" name="email"  value={this.state.email} placeholder="Email" onChange={this.handleChange}/>
                         </Form.Group>
 
                         <Form.Group controlId="formBasicUsername">
                             <Form.Label>Username</Form.Label>
-                            <Form.Control type="username" placeholder="Username" />
-                        </Form.Group>
-                    
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control type="username" name="adminUsername" value={this.state.adminUsername} placeholder="Username" onChange={this.handleChange}/>
                         </Form.Group>
 
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" name="adminPassword" value={this.state.adminPassword} placeholder="Password" onChange={this.handleChange}/>
+                        </Form.Group>
+
+                        {/*<Form.Group controlId="formBasicActiveStatus">
+                            <Form.Label>Active Status</Form.Label>
+                            <Form.Control type="activeStatus" name="activeStatus"  value={this.state.activeStatus} placeholder="Active" onChange={this.handleChange}/>
+                </Form.Group>*/}
+                            
                         <Form.Group controlId="formBasicRadio">
                             <Form.Label>Active Status</Form.Label>
-                            <Form.Check type="radio" label="Active" value='Active' />
-                            <Form.Check type="radio" label="Inactive" value='Inactive' />
+                            <Form.Check type="radio" label="Active" name="activeStatus" value="Active" onChange={this.handleChange} />
+                            <Form.Check type="radio" label="Inactive" name="activeStatus" value="Inactive" onChange={this.handleChange} />
                         </Form.Group>
 
                         <Button variant="primary" type="submit" onClick={()=>this.addAdmin()}> {/*add addAdmin() function */}
